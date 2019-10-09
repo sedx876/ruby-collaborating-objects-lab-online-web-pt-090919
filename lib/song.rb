b352e02bae258bffedde0cf06303516785de14d8
Sharon Watkins <swatkins876@gmail.com>
2019-10-09 17:12:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,9 @@ class Song
   end
 
   def artist_name=(name)
-    guy = Artist.find_or_create_by_name(name)
-    guy.add_song(self)
-    guy
+    artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+    artist 
   end
 
   def self.new_by_filename(filename)
